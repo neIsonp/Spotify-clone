1beb44d1141180eaf253d3dc7337433f92368721
neIsonp <nelsonponte37@gmail.com>
2023-10-08 10:45:23 +0000

    added project image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,6 @@
 import { Footer } from "@/components/Footer";
 import { Main } from "@/components/Main";
 import { SideBar } from "@/components/SideBar";
-import {
-  Home as HomeIcon,
-  Search,
-  Library,
-  ChevronLeft,
-  ChevronRight,
-  Play,
-  Shuffle,
-  SkipBack,
-  SkipForward,
-  Repeat,
-  Mic2,
-  LayoutList,
-  Laptop2,
-  Volume,
-  Maximize2,
-} from "lucide-react";
-import Image from "next/image";
 
 export default function Home() {
   return (
